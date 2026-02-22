b907d02e683796075f90852bef5da40a7f6213f7
arold-takam <www.aroldtakam3@gmail.com>
2026-02-22 15:00:45 +0100

    first deploy stabilizing

--- backend/src/main/java/com/takus/post_it/config/WebConfig.java
+++ backend/src/main/java/com/takus/post_it/config/WebConfig.java
@@ -13,7 +13,11 @@ public class WebConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:5173", "http://localhost:8081")
+					.allowedOrigins(
+						"http://localhost:5173",
+						"http://localhost:8081",
+						"https://postit-page.netlify.app"
+					)
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 					.allowedHeaders("*");
 			}
